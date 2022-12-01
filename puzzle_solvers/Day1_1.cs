f8f145871160a28e23ea542ee09878a24fe58eec
Garrett Rieger <garrettrieger@gmail.com>
2022-12-01 00:28:30 -0600

    Day 1

---
+++ puzzle_solvers/Day1_1.cs
@@ -0,0 +1,38 @@
+
+using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode2022
+{
+    public class Day1_1
+    {
+        public static void SolveDay()
+        {
+            const string input = "puzzle_inputs/day1_1";
+
+            var puzzleInput = InputListGenerator.readInputFileAsIntegers(input);
+
+            var calorieList = new List<Int32>();
+
+            int holder = 0;
+
+            foreach (int i in puzzleInput)
+            {
+                if (i != 0)
+                {
+                    holder += i;
+                }
+                else
+                {
+                    calorieList.Add(holder);
+                    holder = 0;
+                }
+            }
+
+            calorieList.Sort();
+            calorieList.Reverse();
+
+            System.Console.WriteLine(calorieList[0].ToString());
+        }
+    }
+}
